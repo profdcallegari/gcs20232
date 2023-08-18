2727706900fbbd440507d2a807f377a8d55baec0
Prof. Daniel Callegari <daniel.callegari@pucrs.br>
2023-08-18 18:32:06 -0300

    modifiquei o programa

--- App.java
+++ App.java
@@ -2,8 +2,12 @@ public class App
 {
   public static void main (String[] args)
   {
-
+	int x = 1;
+	
     System.out.println("Ola pessoal!");
+
+
+    System.out.println("Tchau, pessoal!");
 
   }
 
